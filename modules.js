1dfdd5cdc0f06fbc06cbf70701d7a3bf0fe45ffa
antonov <antonov@polymetal.ru>
2025-09-04 12:02:54 +0300

    dfg455f1g5f5f

--- modules.js
+++ modules.js
@@ -372,7 +372,7 @@ const setdealfields = (req, res) => {  //Добавление пользоват
             console.log(err);
             res.status(500).send({'message':err});
         }); */
-        console.log("body = " + req.body);
+        console.log(req.body);
         res.status(200).send('Ok!').end();
     }
     
